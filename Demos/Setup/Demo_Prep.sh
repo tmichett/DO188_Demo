325827b1eadaf5d3e438d10ae64818d25fdefffc
Travis Michette <tmichett@redhat.com>
2022-09-29 14:26:19 -0400

    Updated files based on Jenkins build in test.txt

---
+++ Demos/Setup/Demo_Prep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "**********************************************"
+echo "***** Obtain Container Images for Demo *******"
+echo "**********************************************"
+
+podman login registry.ocp4.example.com:8443 -u developer -p developer
+
+podman pull registry.ocp4.example.com:8443/ubi8/ubi
+
+podman pull registry.ocp4.example.com:8443/ubi9/ubi:9.0.0-1468
+
+podman pull registry.ocp4.example.com:8443/ubi8/httpd-24
+
+podman pull registry.ocp4.example.com:8443/redhattraining/hello-world-nginx:v1.0
