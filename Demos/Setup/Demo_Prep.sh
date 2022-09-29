485ef3098d75a0cfe9edc5df5b0cdc94a8f18f05
Travis Michette <tmichett@redhat.com>
2022-09-29 15:26:20 -0400

    Updated files based on Jenkins build in test.txt

--- Demos/Setup/Demo_Prep.sh
+++ Demos/Setup/Demo_Prep.sh
@@ -8,6 +8,8 @@ podman login registry.ocp4.example.com:8443 -u developer -p developer
 
 podman pull registry.ocp4.example.com:8443/ubi8/ubi
 
+podman pull registry.ocp4.example.com:8443/ubi8/ubi-minimal:8.5
+
 podman pull registry.ocp4.example.com:8443/ubi9/ubi:9.0.0-1468
 
 podman pull registry.ocp4.example.com:8443/ubi8/httpd-24
