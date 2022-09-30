c73d0a600b11d72499d9899763ce93f7888a0170
Travis Michette <tmichett@redhat.com>
2022-09-30 10:56:22 -0400

    Updated files based on Jenkins build in test.txt

--- Demos/Setup/Demo_Prep.sh
+++ Demos/Setup/Demo_Prep.sh
@@ -15,3 +15,12 @@ podman pull registry.ocp4.example.com:8443/ubi9/ubi:9.0.0-1468
 podman pull registry.ocp4.example.com:8443/ubi8/httpd-24
 
 podman pull registry.ocp4.example.com:8443/redhattraining/hello-world-nginx:v1.0
+
+
+echo "**********************************************"
+echo "******* Cloning OCP_Demo Repository **********"
+echo "**********************************************"
+
+cd ~/Github
+git clone https://github.com/tmichett/OCP_Demos.git
+cd 
