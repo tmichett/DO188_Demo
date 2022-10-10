013ace2c27284d99dd4293bede7ca0e37814818a
Travis Michette <tmichett@redhat.com>
2022-10-10 18:18:55 -0400

    Updated files based on Jenkins build in test.txt

---
+++ Demos/CH4/Advanced_Containerfile/Run_Container.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+#
+# Travis Michette <tmichett@redhat.com>
+
+set -e
+
+container='quay.io/tmichett/asciidocpdf:latest'
+pdf_theme='-a pdf-theme=/opt/asciidoc/themes/tm-gls-redhat-theme.yml'
+current_directory="$(pwd)"
+
+
+echo "Command being run is: podman run --name adocpdf --rm -v $current_directory:/tmp/coursebook:Z $container $pdf_theme $1"
+
+podman run --name adocpdf --rm -v $current_directory:/tmp/coursebook:Z $container $pdf_theme $1
